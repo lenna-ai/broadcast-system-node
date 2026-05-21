c484310ada17acadd096486ca11d7221cae129d7
dwijulians <dimas@lenna.ai>
2026-05-21 14:34:40 +0700

    ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -47,23 +47,23 @@ module.exports = {
       env_production: {
         NODE_ENV: "production"
       }
-    }
-    // {
-    //   name: "broadcast-server",
-    //   script: "./server.js", // Merujuk ke baris 7 di image_4770c0.png
-    //   instances: 2, // Anda bisa sesuaikan jumlah worker untuk memproses antrean lebih cepat
-    //   exec_mode: "cluster",
-    //   autorestart: true,
-    //   watch: false,
-    //   max_memory_restart: '1G',
-    //   env_local: {
-    //     NODE_ENV: "local",
-    //     DOTENV_CONFIG_PATH: "./.env"
-    //   },
-    //   env_production: {
-    //     NODE_ENV: "production",
-    //     DOTENV_CONFIG_PATH: "./.env"
-    //   }
-    // },
+    },
+    {
+      name: "broadcast-server",
+      script: "./server.js", // Merujuk ke baris 7 di image_4770c0.png
+      instances: 1, // Anda bisa sesuaikan jumlah worker untuk memproses antrean lebih cepat
+      exec_mode: "fork",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env_local: {
+        NODE_ENV: "local",
+        DOTENV_CONFIG_PATH: "./.env"
+      },
+      env_production: {
+        NODE_ENV: "production",
+        DOTENV_CONFIG_PATH: "./.env"
+      }
+    },
   ]
 };
